b9bbc10b78d79c9847d38c11373d477ffe22ce86
bl4ckmore <blackmorevar@gmail.com>
2025-05-25 19:41:47 +0400

    V3

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import { CartProvider } from '../context/CartContext';
 import { AuthProvider } from '../context/AuthContext';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 export default function App({ Component, pageProps }) {
   return (
